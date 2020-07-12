91f5e0b118183b95104cabccd6939bbf2d41f9c6
PikaKight <tmmarcus73@gmail.com>
2020-07-11 22:13:30 -0400

    more testing

--- test_physic.py
+++ test_physic.py
@@ -1,4 +1,9 @@
-import physic
+from physic import *
 
 def test_base_velocity_equation():
-   assert Base_velocity_equation.velocity(5.0,5.0) == 1.0
+   assert Base_velocity_equation().velocity(5.0,5.0) == 1.0
+   assert Base_velocity_equation().distance(5.0,5.0) == 25.0
+   assert Base_velocity_equation().time(5.0,5.0) == 1.0
+
+def test_no_distance():
+   assert No_distance().velocity_final(5,6,1) == 11
